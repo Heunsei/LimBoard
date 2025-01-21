0b4da8ff6b9deead52f1a8cf9eaf0f915722aa96
이흔오 <gms245@naver.com>
2025-01-22 01:42:36 +0900

    feat: user icon

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,8 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { UserButton } from "@/features/auth/components/user-button";
 export default function Home() {
   return (
     <div className="flex gap-4">
-      <Input />
-      <Button size="sm">click here</Button>
-      <Button variant="destructive">click here</Button>
-      <Button variant="secondary">click here</Button>
-      <Button variant="ghost">click here</Button>
-      <Button variant="muted">muted</Button>
-      <Button variant="teritary">teritary</Button>
+      <UserButton />
     </div>
   );
 }
