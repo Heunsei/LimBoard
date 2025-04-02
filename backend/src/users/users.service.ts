d702fd90ea24be3d3f98a24818e63237f2a845a1
heunoh <heunoh01@gmail.com>
2025-04-02 22:17:36 +0900

    feat : check create user

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,11 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  async createUser(createUserDto: CreateUserDto) {
+    const prisma = new PrismaClient()
+    const createUser = await prisma.user.create({
+      data : {
+        ...createUserDto
+      }}
+    )
+    return createUser;
   }
 
   findAll() {
